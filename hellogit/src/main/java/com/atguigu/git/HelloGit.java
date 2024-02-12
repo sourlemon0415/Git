71476500f0e37f23bb665d5f66e482adcbcc4cff
lemon <lemon_bird@outlook.com>
2024-02-12 14:17:36 +0800

    添加文件

--- hellogit/src/main/java/com/atguigu/git/HelloGit.java
+++ hellogit/src/main/java/com/atguigu/git/HelloGit.java
@@ -8,6 +8,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git!");
-
+        System.out.println("add");
     }
 }
